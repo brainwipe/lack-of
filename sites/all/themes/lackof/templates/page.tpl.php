02bccaca3f7c31c2897c3e6f6d7f05040edec157
Rob Lang <brainwiped@gmail.com>
2016-06-10 20:13:22 +0100

    Added line between comments and main post, moved the odd dividing line from the top and reduced size of text. Fixes #1

--- sites/all/themes/lackof/templates/page.tpl.php
+++ sites/all/themes/lackof/templates/page.tpl.php
@@ -117,16 +117,7 @@
 
 <div class="main-container container">
 
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-
   <div class="row">
-
     <?php if (!empty($page['sidebar_first'])): ?>
       <aside class="col-sm-3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
