f7fe62db1efd4565bf5bb8a1f4bfe881de4c6fbe
Rob Lang <brainwiped@gmail.com>
2016-06-16 20:05:13 +0100

    Added FontAwesome and ensured that the html had the correct headers. Fixes #17

--- sites/all/themes/lackof/templates/html.tpl.php
+++ sites/all/themes/lackof/templates/html.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * Default theme implementation to display the basic html structure of a single
@@ -9,7 +8,18 @@
  * - $css: An array of CSS files for the current page.
  * - $language: (object) The language the site is being displayed in.
  *   $language->language contains its textual representation.
- *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
+ *   $language->dir contains the language direction. It will either be 'ltr' or
+ *   'rtl'.
+ * - $html_attributes:  String of attributes for the html element. It can be
+ *   manipulated through the variable $html_attributes_array from preprocess
+ *   functions.
+ * - $html_attributes_array: An array of attribute values for the HTML element.
+ *   It is flattened into a string within the variable $html_attributes.
+ * - $body_attributes:  String of attributes for the BODY element. It can be
+ *   manipulated through the variable $body_attributes_array from preprocess
+ *   functions.
+ * - $body_attributes_array: An array of attribute values for the BODY element.
+ *   It is flattened into a string within the variable $body_attributes.
  * - $rdf_namespaces: All the RDF namespace prefixes used in the HTML document.
  * - $grddl_profile: A GRDDL profile allowing agents to extract the RDF data.
  * - $head_title: A modified version of the page title, for use in the TITLE
@@ -36,22 +46,28 @@
  * - $classes String of classes that can be used to style contextually through
  *   CSS.
  *
+ * @see bootstrap_preprocess_html()
  * @see template_preprocess()
  * @see template_preprocess_html()
  * @see template_process()
  *
- * @ingroup themeable
+ * @ingroup templates
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
-
-<head profile="<?php print $grddl_profile; ?>">
+?><!DOCTYPE html>
+<html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
+<head>
+  <link rel="profile" href="<?php print $grddl_profile; ?>" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
+  <!-- HTML5 element support for IE6-8 -->
+  <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
   <?php print $scripts; ?>
-  <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+   <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
 <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
@@ -72,8 +88,9 @@
 <meta name="msapplication-TileColor" content="#e7e1d4">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <meta name="theme-color" content="#ffffff">
+  <script src="https://use.fontawesome.com/229c0dfd7f.js"></script>
 </head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<body<?php print $body_attributes; ?>>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
